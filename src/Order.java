90007af05b5ce2761aa489f327beed2c758ddd2e
Tebini <chaima_tebini@hotmail.com>
2015-12-17 11:58:33 +0100

    ändring

--- src/Order.java
+++ src/Order.java
@@ -1,44 +1,43 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Order {
 	
-	private String ordernr;
-	private String leveransadress;
-	private Kund owner;
-	private ArrayList<Orderrad>orderrader= new ArrayList<Orderrad>();
-	private ArrayList<Produkt> orderprodukter = new ArrayList<Produkt>();
+	private String orderNumber;
+	private String deliveryAdress;
+	private String orderDate;
+	private Customer holder;
+	private HashMap<String, OrderLine> orderlines= new HashMap<String, OrderLine>();
 	
-	public String getOrdernr() {
-		return ordernr;
+	public String getOrderNumber() {
+		return orderNumber;
 	}
-	public void setOrdernr(String ordernr) {
-		this.ordernr = ordernr;
+	public void setOrderNumber(String orderNumber) {
+		this.orderNumber = orderNumber;
 	}
-	public String getLeveransadress() {
-		return leveransadress;
+	public String getDeliveryAdress() {
+		return deliveryAdress;
 	}
-	public void setLeveransadress(String leveransadress) {
-		this.leveransadress = leveransadress;
+	public void setDeliveryAdress(String deliveryAdress) {
+		this.deliveryAdress = deliveryAdress;
+	}
+	public String getOrderDate() {
+		return orderDate;
+	}
+	public void setOrderDate(String orderDate) {
+		this.orderDate = orderDate;
+	}
+	public Customer getHolder() {
+		return holder;
+	}
+	public void setHolder(Customer holder) {
+		this.holder = holder;
+	}
+	public HashMap<String, OrderLine> getOrderlines() {
+		return orderlines;
+	}
+	public void setOrderlines(HashMap<String, OrderLine> orderlines) {
+		this.orderlines = orderlines;
 	}
 	
-	public void setOwner(Kund owner){
-		this.owner= owner;
-	}
-	public Kund getOwner(){
-		return owner;
-	}
-	public ArrayList<Orderrad> getOrderrader() {
-		return orderrader;
-	}
-	public void setOrderrader(ArrayList<Orderrad> orderrader) {
-		this.orderrader = orderrader;
-	}
-	public ArrayList<Produkt> getOrderprodukter() {
-		return orderprodukter;
-	}
-	public void setOrderprodukter(ArrayList<Produkt> orderprodukter) {
-		this.orderprodukter = orderprodukter;
-	}
 	
-
 }
