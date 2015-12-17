6722e53af2726ebe0f1ca05f5fd2fd167f147fd1
Tebini <chaima_tebini@hotmail.com>
2015-12-17 11:31:40 +0100

    ändring

---
+++ src/Customer.java
@@ -0,0 +1,57 @@
+import java.util.HashMap;
+
+public class Customer {
+	
+	private String customerNumber;
+	private String name;
+	private String lastName;
+	private String billingAddress;
+	private String emailAddress;
+	private String phoneNumber;
+	private HashMap<String, Order> orders = new HashMap<String, Order>();
+	
+	
+	public String getCustomerNumber() {
+		return customerNumber;
+	}
+	public void setCustomerNumber(String customerNumber) {
+		this.customerNumber = customerNumber;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getBillingAddress() {
+		return billingAddress;
+	}
+	public void setBillingAddress(String billingAddress) {
+		this.billingAddress = billingAddress;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public HashMap<String, Order> getOrders() {
+		return orders;
+	}
+	public void setOrders(HashMap<String, Order> orders) {
+		this.orders = orders;
+	}
+
+}
